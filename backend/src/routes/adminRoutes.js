aa50a84ba82d331770a28f13c54c65eb16663b9a
MThanh <doanminhthanh0302@gmail.com>
2025-11-11 00:32:16 +0700

    update routeRoutes

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -9,7 +9,7 @@ import {
 } from '../controllers/adminController.js';
 
 router.get('/users', authenticateToken, admin, getAllUsers);
-router.get('/status', authenticateToken, admin, getDashboardStats);
+router.get('/stats', authenticateToken, admin, getDashboardStats);
 router.get('/topics', authenticateToken, admin, getAllTopics);
 router.get('/words', authenticateToken, admin, getAllWords);
 
